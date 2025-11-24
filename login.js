f754ad764cd04458aa1b4ca4aade5a605763da74
geff <geffery.addai1@ucalgary.ca>
2025-11-23 19:09:48 -0700

    added remember me frontend feature

---
+++ login.js
@@ -0,0 +1,30 @@
+//on click
+
+const users = {
+    geffery1: { password: "1234" },
+    testUser: { password: "abcd1234" }
+};
+
+
+// document.addEventListener("click", (e) => login()) runs click anytime
+// works
+function login(){
+    let username = document.getElementById("Username-input").value;
+    let password = document.getElementById("Password-input").value;
+    if (username in users && password === users[username].password) {
+       console.log("tester tester ");
+       console.log(username);
+        console.log(password);
+    }
+    else{
+
+    }
+}
+
+function signup(){
+  //  let username = document.getElementById("").value
+    // let password = document.getElementById("").value
+
+}
+signup()
+
